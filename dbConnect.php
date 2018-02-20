c10b14dc0dbb98012a136cf9ed37f29fe73e04a2
João Paulo Marques Votri <jpaulo.votri@hotmail.com>
2018-02-20 02:37:17 -0300

    Ajustado para conexao heroku

--- dbConnect.php
+++ dbConnect.php
@@ -1,11 +1,17 @@
 <?php
 
 function getDb() {
-  $host = "localhost";
+  /*$host = "localhost";
   $port = "5432";
   $dbname = "votricrum";
   $user = "postgres";
-  $password = "postgres";
+  $password = "postgres";*/
+
+  $host = "ec2-54-163-234-99.compute-1.amazonaws.com";
+  $port = "5432";
+  $dbname = "d23pmlmrk0m2v1";
+  $user = "prqocqtrazgsbj";
+  $password = "7d645efc88f6521a97ee905fd71d57333f91334b9687c1ded64bdf440695457e";
 
  $connectionString = "host=$host port=$port dbname=$dbname user=$user password=$password";
 
